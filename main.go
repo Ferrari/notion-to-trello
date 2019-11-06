e457ae0bdb66d8e22c2c5ad18ff549cd53cf475b
David Hayes <drhayes@gmail.com>
2019-11-06 15:43:02 -0600

    Look, Ma, go code!
    This basically worked and moved my exported MD files to Trello. Didn't
    have to handle rate-limiting and I'll probably end up doing some content
    cleanup anyway, but the bulk work is done.

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"strings"
 
-	trello "github.com/adlio/trello"
+	"github.com/adlio/trello"
 	blackfriday "gopkg.in/russross/blackfriday.v2"
 )
+
+type cardData struct {
+	title    string
+	status   string
+	contents string
+}
+
+func (card cardData) String() string {
+	return fmt.Sprintf("Title: %s\nStatus: %s\nContents: %s\n", card.title, card.status, card.contents)
+}
+
+var currentCard cardData
 
 func check(e error) {
 	if e != nil {
@@ -17,8 +31,28 @@ func check(e error) {
 	}
 }
 
+const statusPrefix = "Status: "
+
 func visitor(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
-	log.Println(node.Type, entering, string(node.Literal))
+	// If we found a heading and we don't have a title yet, that's our title.
+	if len(currentCard.title) == 0 && node.Type == blackfriday.Heading {
+		firstChild := *node.FirstChild
+		currentCard.title = string(firstChild.Literal)
+		return blackfriday.SkipChildren
+	}
+	if len(currentCard.contents) == 0 && node.Type == blackfriday.Text {
+		currentCard.contents = string(node.Literal)
+		// Make a scanner to find the status line.
+		scanner := bufio.NewScanner(strings.NewReader(currentCard.contents))
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.HasPrefix(line, statusPrefix) {
+				currentCard.status = strings.TrimSpace(strings.TrimPrefix(line, statusPrefix))
+			}
+		}
+		return blackfriday.SkipChildren
+	}
+	// Nothing to see here, move along...
 	return blackfriday.GoToNext
 }
 
@@ -34,6 +68,19 @@ func main() {
 		panic("todoDir is required")
 	}
 
+	filenames, err := ioutil.ReadDir(todoDir)
+	check(err)
+	var cards []cardData
+	for _, filename := range filenames {
+		contents, err := ioutil.ReadFile(path.Join(todoDir, filename.Name()))
+		check(err)
+		parser := blackfriday.New()
+		parsed := parser.Parse(contents)
+		currentCard = cardData{}
+		parsed.Walk(visitor)
+		cards = append(cards, currentCard)
+	}
+
 	client := trello.NewClient(appKey, token)
 	log.Println("About to retrieve board...")
 	board, err := client.GetBoard(boardID, trello.Defaults())
@@ -46,18 +93,21 @@ func main() {
 	var listMap map[string]*trello.List
 	listMap = make(map[string]*trello.List)
 	for _, list := range lists {
-		listMap[strings.ToLower(list.Name)] = list
+		listMap[list.Name] = list
 	}
 	log.Println(listMap)
 
-	filenames, err := ioutil.ReadDir(todoDir)
-	check(err)
-	for _, filename := range filenames {
-		contents, err := ioutil.ReadFile(path.Join(todoDir, filename.Name()))
-		check(err)
-		parser := blackfriday.New()
-		parsed := parser.Parse(contents)
-		parsed.Walk(visitor)
-		panic(3)
+	for _, card := range cards {
+		list := listMap[card.status]
+		if list == nil {
+			panic("Unknown status: " + card.status)
+		}
+		trelloCard := trello.Card{
+			Name: card.title,
+			Desc: card.contents,
+		}
+		log.Printf("Adding %q...\n", card.title)
+		list.AddCard(&trelloCard, trello.Defaults())
 	}
+
 }
